1706338dd2462f58a954e649a71c80ba3072acd0
Lucas Gabriel Bernardino <122226174+lucas-bernardino@users.noreply.github.com>
2024-04-08 15:27:51 -0300

    Add post_video logic

--- codigo_sensor_rust/src/main.rs
+++ codigo_sensor_rust/src/main.rs
@@ -1,6 +1,9 @@
 use std::process::{Command, Stdio};
 use serde::{Deserialize, Serialize};
 
+use reqwest::header::{CONTENT_TYPE, CONTENT_DISPOSITION};
+
+use tokio_util::io::ReaderStream;
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Body {
@@ -8,7 +11,7 @@ struct Body {
 }
 
 const MIN_DECIBEL: u16 = 700;
-const MIN_DECIBEL_STOP: u16 = 620;
+const MIN_DECIBEL_STOP: u16 = 580;
 
 #[tokio::main(flavor = "current_thread")]
 #[allow(unreachable_code)]
@@ -33,26 +36,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut file_count: u16 = 0;
 
     match check_camera_available() {
-        Ok(_) => println!("Found camera."), 
+        Ok(_) => println!("Found camera."),
         Err(e) => panic!("ERROR: {e}") // TODO: Maybe create a POST method that will return the error.
     };
 
     loop {
         let sensor_data = ctx.read_holding_registers(0x00, 2).await?;
         let decibels_value = ( sensor_data[0] + sensor_data[1] ) / 2;
-        
+
         let body = Body { decibels: decibels_value };
-        
+        /*
         let res = server.post("http://localhost:3000/create")
             .json(&body)
             .send()
             .await?;
-
+*/
         println!("Current decibels: {decibels_value}");
 
         if decibels_value > MIN_DECIBEL && !is_recording {
-            match start_recording(&mut file_count) {
-                Ok(pid) => {ffmpeg_pid = pid},
+            match start_recording() {
+                Ok(pid) => {
+                    ffmpeg_pid = pid;
+                    println!("Started recording!");
+                },
                 Err(err) => panic!("ERROR: could not start recording\n{err}") // TODO: Maybe create a POST method that will return the error.
             };
             is_recording = true;
@@ -63,21 +69,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 Ok(_) => {
                     println!("Stopped recording ffmpeg with PID: {ffmpeg_pid}");
                     is_recording = false;
+                    post_video(&server).await;
                 },
                 Err(err) => {
                     panic!("ERROR: could not stop recording\n{err}"); // TODO: Maybe create a POST method that will return the error.
-                } 
+                }
             };
-            
+
         }
-        
+
     }
 
     Ok(())
 }
 
 fn check_camera_available() -> Result<(), Box<dyn std::error::Error>> {
-    
+
     let v4l2_command = Command::new("v4l2-ctl")
         .arg("--list-devices")
         .output()?;
@@ -87,24 +94,20 @@ fn check_camera_available() -> Result<(), Box<dyn std::error::Error>> {
     if output.len() != 0 {
         return Err(output.into());
     }
-    
+
     Ok(())
 }
 
-fn start_recording(count: &mut u16) -> Result<u32, Box<dyn std::error::Error>>  {
-    
-    let format_count = format!("out{}.mkv", count);
+fn start_recording() -> Result<u32, Box<dyn std::error::Error>>  {
 
     let ffmpeg_command = Command::new("ffmpeg")
-        .args(["-f", "v4l2", "-framerate", "30", "-video_size", "1280x720", "-input_format", "mjpeg", "-i", "/dev/video0", "-c", "copy", &format_count])
+        .args(["-f", "v4l2", "-framerate", "30", "-video_size", "1280x720", "-input_format", "mjpeg", "-i", "/dev/video0", "-c", "copy", "out.mkv"])
         .stdout(Stdio::null())
         .stderr(Stdio::null())
         .stdin(Stdio::null())
         .spawn();
 
     let id = ffmpeg_command?.id();
-
-    *count += 1;
 
     Ok(id)
 }
@@ -116,5 +119,34 @@ fn stop_recording(ffmpeg_pid: u32) -> Result<(), Box<dyn std::error::Error>> {
         .spawn()?;
 
     Ok(())
-    
+
+}
+
+async fn post_video(server: &reqwest::Client) -> Result<(), Box<dyn std::error::Error>> {
+
+    println!("Inside of post_video!");
+
+    let file = tokio::fs::File::open("out.mkv").await.unwrap();
+
+    let stream = ReaderStream::new(file);
+
+
+    let body_stream = reqwest::Body::wrap_stream(stream);
+
+
+    let response = server.post("http://localhost:3000/video")
+        .header(CONTENT_TYPE, "video/webm")
+        .header(CONTENT_DISPOSITION, "attachment; filename=out.mkv")
+        .body(body_stream)
+        .send();
+
+    Command::new("rm")
+        .arg("out.mkv")
+        .spawn()?;
+
+
+    println!("Response is: {x:#?}", x = response.await);
+
+    Ok(())
+
 }
