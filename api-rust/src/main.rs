1b09e13800acddee7ccc259ddcc71c60dd4bcc20
lucas-bernardino <lucasgber@gmail.com>
2024-07-08 14:13:14 +0100

    Add header in http request

--- api-rust/src/main.rs
+++ api-rust/src/main.rs
@@ -108,7 +108,7 @@ async fn main() {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 
-    println!("Server running sucessfully on port 3000 ✅");
+    //println!("Server running sucessfully on port 3000 ✅");
 
     script_check.await.unwrap();
 }
