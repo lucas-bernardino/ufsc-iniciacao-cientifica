85f6635adefe2e1b5cc967aebc83af71063d2b44
lucas-bernardino <lucasgber@gmail.com>
2024-08-01 09:18:15 -0300

    Update mock with socket.io

--- mock_client_socket/src/main.rs
+++ mock_client_socket/src/main.rs
@@ -1,3 +1,5 @@
+use std::i32;
+
 use rust_socketio::{
     asynchronous::{Client, ClientBuilder},
     Payload,
@@ -6,9 +8,16 @@ use serde::{Deserialize, Serialize};
 
 use futures_util::{FutureExt, SinkExt, StreamExt};
 use tokio_tungstenite::{self, tungstenite::Message};
+
 #[derive(Debug, Deserialize, Serialize)]
 struct MockBody {
     decibels: u16,
+}
+
+#[derive(Deserialize)]
+struct UpdateResponse {
+    min: u16,
+    max: u16,
 }
 
 //const MIN_DECIBEL: u16 = 700;
@@ -20,27 +29,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
     let callback = |payload: Payload, socket: Client| {
         async move {
+            let mut data = String::from("");
             match payload {
-                Payload::Text(str) => println!("Received: {:#?}", str),
-                Payload::Binary(bin_data) => println!("Received bytes: {:#?}", bin_data),
-                _ => println!("Outro"),
+                Payload::Text(text) => data = text.first().unwrap().to_string(),
+                _ => {}
             }
-            //socket
-            //    .emit("message", "risos")
-            //    .await
-            //    .expect("Server unreachable")
+            let parsed_data: UpdateResponse = serde_json::from_str(data.as_str()).unwrap();
+            let min = parsed_data.min;
+            let max = parsed_data.max;
+            println!("Sucessfully got: {min} and {max}");
         }
         .boxed()
     };
     let socket = ClientBuilder::new("http://127.0.0.1:3000")
         .namespace("")
-        .on("message", callback)
+        .on("update", callback)
         .connect()
         .await
         .expect("Connection failed");
 
     std::thread::sleep(std::time::Duration::from_secs(3));
-    socket.emit("message", "mic").await.unwrap();
     loop {
         for i in 1..100 {
             let mock_body = MockBody { decibels: i * 10 };
